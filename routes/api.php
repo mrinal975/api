b0e69780ba5decaf48d44434b28bf6b96c0121cd
mrinal975 <mrinalmallik1@gmail.com>
2019-09-23 18:07:18 +0600

    testCommit

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::apiResource('products','ProductController');
 
+Route::apiResource('productss','ProductControllers');
+
 Route::group(['prefix'=>'products'],function (){
     Route::apiResource('/{product}/reviews','ReviewController');
 });
